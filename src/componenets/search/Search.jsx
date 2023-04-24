bea6e979a70822c39b12904f4ca17da2f68f7704
Aaron Morse <morse.aaron1985@gmail.com>
2023-04-24 14:09:50 -0700

    added the error message for submitting a blank query

--- src/componenets/search/Search.jsx
+++ src/componenets/search/Search.jsx
@@ -4,36 +4,53 @@ import searchIcon from "../../assets/images/icon-search.svg";
 
 export default function Search({ setData }) {
   const [query, setQuery] = useState("");
+  const [error, setError] = useState(false);
 
   async function fetchData(e) {
     e.preventDefault();
+    if (!query) {
+      setError(true);
+      return;
+    }
+
     try {
       const response = await fetch(
         `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`
       );
-      const data = await response.json();
-      setData(data[0]);
+      const json = await response.json();
+      const [data] = json;
+      setData(data);
     } catch (err) {
       console.log("error", err);
     }
   }
 
   return (
-    <form
-      onSubmit={fetchData}
-      onFocus={(e) => e.target.select()}
-      className={styles.form}
-    >
-      <input
-        type="text"
-        placeholder="Search for any word..."
-        className={styles.searchInput}
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button className={styles.searchBtn}>
-        <img src={searchIcon} />
-      </button>
-    </form>
+    <>
+      <form
+        onSubmit={fetchData}
+        onFocus={(e) => e.target.select()}
+        className={
+          error ? `${styles.form} ${styles.error}` : `${styles.form}`
+        }
+      >
+        <input
+          type="text"
+          placeholder="Search for any word..."
+          className={styles.searchInput}
+          value={query}
+          onChange={(e) => {
+            setError(false);
+            setQuery(e.target.value);
+          }}
+        />
+        <button className={styles.searchBtn}>
+          <img src={searchIcon} />
+        </button>
+      </form>
+      {error && (
+        <p className={styles.errorMsg}>Whoops, can't be empty...</p>
+      )}
+    </>
   );
 }
