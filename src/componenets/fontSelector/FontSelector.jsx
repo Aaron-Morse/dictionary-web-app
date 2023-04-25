7edda83228d07e21766fb9372287c5156f6b8aec
Aaron Morse <morse.aaron1985@gmail.com>
2023-04-25 13:42:02 -0700

    fixed the font selector modal to be fixed to the parent container

--- src/componenets/fontSelector/FontSelector.jsx
+++ src/componenets/fontSelector/FontSelector.jsx
@@ -30,14 +30,14 @@ export default function FontSelector({ font, setFont }) {
   }
 
   return (
-    <>
-      <div className={styles.container} onClick={toggleModal}>
+    <div className={styles.container}>
+      <div onClick={toggleModal}>
         <span className={styles.fontType}>
           {fontType[fontName.indexOf(font)]}
         </span>
         <img src={arrowDownIcon} alt="Down arrow icon" />
       </div>
       {showFontModal && <FontModal />}
-    </>
+    </div>
   );
 }
